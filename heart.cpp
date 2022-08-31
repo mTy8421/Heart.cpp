ecc746863f974c1481c068a194df98e198471417
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2022-08-31 22:40:44 +0700

    Update heart.cpp

--- heart.cpp
+++ heart.cpp
@@ -27,5 +27,13 @@ int main()
         cout << endl;
     }
 
+
+    for(int i = size; i > 0; i--){
+        for(int j = 0; j < size - i; j++){
+            cout << " ";
+        }
+    }
+    
+
     return EXIT_SUCCESS;
 }

e7738b29e63e73a88af92a84fa859717278e00a1
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2022-08-31 22:35:06 +0700

    Update heart.cpp

--- heart.cpp
+++ heart.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    
-    
+    int size = 5;
+
+    for (int i = size / 2; i < size; i += 2)
+    {
+        for (int i = 1; i < size - 1; i += 2)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j < size - i + 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     return EXIT_SUCCESS;
 }
